91f0304b72390f703d863dcba784393b167948c3
emilrichardo <emilrichardo@gmail.com>
2022-04-01 20:29:27 -0300

    env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    restApi : "https://staging.disruptivenews.io/",
+    tokenApi  : "https://api.pancakeswap.info/api/v2/tokens/",
+    contractToken :"0xd70a9D1c9fDd3D8DD2fb672bB399F7bcA61666bD"
+  }
 }
 
 module.exports = nextConfig
