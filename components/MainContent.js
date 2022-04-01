ffe92bde7d27a60a02707817c1f017a0ca75d557
emilrichardo <emilrichardo@gmail.com>
2022-04-01 20:30:43 -0300

    main content height

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,6 +1,6 @@
 export default function MainContent({className, children}) {
     return(
-        <main className={` ${className} pt-20  px-8 lg:px-24  min-h-full overflow-x-hidden box-border`}>
+        <main className={` ${className}  pt-20  px-8 lg:px-24  min-h-[50vh] overflow-x-hidden box-border`}>
             {children}
         </main>
     )
