7f5e582c0b5c654733a6bf123eba96035963285d
emilrichardo <emilrichardo@gmail.com>
2022-04-05 14:05:03 -0300

    min height remove

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,6 +1,6 @@
 export default function MainContent({className, children}) {
     return(
-        <main className={` max-w-[1440px] mx-auto  pt-20  px-8 lg:px-24  min-h-[50vh] overflow-x-hidden box-border ${className} `}>
+        <main className={`${className && className} max-w-[1440px] mx-auto  pt-20  px-8 lg:px-24  overflow-x-hidden box-border  `}>
             {children}
         </main>
     )
