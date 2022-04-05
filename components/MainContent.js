65a86e94511e5dd1a5d4c530bb3afae35ca878cb
emilrichardo <emilrichardo@gmail.com>
2022-04-05 14:02:50 -0300

    axwidth content main

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,6 +1,6 @@
 export default function MainContent({className, children}) {
     return(
-        <main className={` ${className}  pt-20  px-8 lg:px-24  min-h-[50vh] overflow-x-hidden box-border`}>
+        <main className={` max-w-[1440px] mx-auto  pt-20  px-8 lg:px-24  min-h-[50vh] overflow-x-hidden box-border ${className} `}>
             {children}
         </main>
     )
