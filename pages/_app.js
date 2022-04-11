02647fe3dd6493e5710dae9fdf0478b12de21f18
emilrichardo <emilrichardo@gmail.com>
2022-04-11 16:28:31 -0300

    metas

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+
 import Layout from '../components/Layout'
 import { appWithTranslation } from 'next-i18next'
 import { TokenProvider } from '../context/tokenContext'
@@ -12,12 +12,7 @@ function MyApp({ Component, pageProps, router }) {
 
   return (
       <Layout>
-        <Head>
-          <meta name="description" content="At Disruptive we reinvent the world. We create and innovate in a way that some people can't even imagine and we bet on projects that are out of the ordinary: that makes us unique. It makes us Disruptive." />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-
-        </Head>
 
         <TokenProvider>
           <AnimatePresence exitBeforeEnter intial={false}>
